a302313dfdb4413d952ca595726dc3efabecd9ef
S CHARAN KUMAR <charans2605@gmail.com>
2025-07-13 13:03:19 +0530

    Create Problem-4.py

---
+++ Problem-4.py
@@ -0,0 +1,8 @@
+def total_nums(l1, l2):
+    c1 = len(l1)
+    c2 = len(l2)
+    total = c1 + c2
+    return total
+l1 = [0, 2, 4, 4, 5, 9, 7]
+l2 = [4, 6, 9]
+print("total no of elements:", total_nums(l1, l2))
