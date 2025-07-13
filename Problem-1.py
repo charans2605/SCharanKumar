7ae1c0931865ad63f1c9882b1957d9bbdfad0df5
S CHARAN KUMAR <charans2605@gmail.com>
2025-07-13 21:11:38 +0530

    Update Problem-1.py

--- Problem-1.py
+++ Problem-1.py
@@ -1,25 +1,27 @@
-def calci(num1, num2, oper):
-    if oper == '+':
-        return num1 + num2
-    elif oper == '-':
-        return num1 - num2
-    elif oper == '*':
-        return num1 * num2
-    elif oper == '/':
-        if num2 != 0:
-            return num1 / num2
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a 
+        self.b = b  
+
+    def calculate(self, operation):
+        if operation == 'add':
+            return self.a + self.b
+        elif operation == 'sub':
+            return self.a - self.b
+        elif operation == 'mul':
+            return self.a * self.b
+        elif operation == 'div':
+            if self.b != 0:
+                return self.a / self.b
+            else:
+                return "Can not divide by zero"
         else:
-            return "Can not divide by the zero"
-    elif oper == '%':
-        if num2 != 0:
-            return num1 % num2
-        else:
-            return "Can not perform the modulus operation with zero"
-    else:
-        return "invalid operation"
-    
-number1 = float(input("enter 1st number: "))
-number2 = float(input("enter 2nd number: "))
-op = input("enter operation (+, -, *, /, %): ")
-res = calci(number1, number2, op)
+            return "invalid operation"
+
+a = float(input("enter 1st number: "))
+b = float(input("enter 2nd number: "))
+operation = input("enter operation (add, sub, mul, div): ").lower()
+
+calc = Calculator(a, b)
+res = calc.calculate(operation)
 print("result:", res)
